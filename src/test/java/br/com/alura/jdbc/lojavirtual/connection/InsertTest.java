12dcbcc0d6eae9c37dfbcca7b442dfb3474eb1c2
Paulo Ferreira <phgferreira@outlook.com>
2022-10-18 20:30:30 -0300

    Fazendo com que o InsertTest delete os registros que inseriu para teste

--- src/test/java/br/com/alura/jdbc/lojavirtual/connection/InsertTest.java
+++ src/test/java/br/com/alura/jdbc/lojavirtual/connection/InsertTest.java
@@ -25,7 +25,10 @@ public class InsertTest {
 
     @AfterEach
     void afterEach() {
+        String deleteScript = "delete from produto where nome like 'PRODUTO_TESTE_%'";
         try {
+            Statement statement = connection.createStatement();
+            statement.execute(deleteScript);
             connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -34,7 +37,7 @@ public class InsertTest {
 
     @Test @DisplayName("Deve inserir um novo produto na tabela produto do banco de dados")
     void insertTest() {
-        String script = "insert into produto (nome, descricao) values ('Mouse', 'Mouse sem fio')";
+        String script = "insert into produto (nome, descricao) values ('PRODUTO_TESTE_0', 'Mouse sem fio')";
         try {
             Statement statement = connection.createStatement();
             statement.execute(script, Statement.RETURN_GENERATED_KEYS);
@@ -55,10 +58,10 @@ public class InsertTest {
         String script = "insert into produto (nome, descricao) values (?, ?)";
         try {
             PreparedStatement statement = connection.prepareStatement(script, Statement.RETURN_GENERATED_KEYS);
-            ResultSet result1 = insereProduto(statement, "Smart TV", "45 polegadas");
-            ResultSet result2 = insereProduto(statement, "Radio", "Radio de bolso");
-            ResultSet result3 = insereProduto(statement, "Computador", "Gabinete");
-            ResultSet result4 = insereProduto(statement, "Monitor", "13 polegadas");
+            ResultSet result1 = insereProduto(statement, "PRODUTO_TESTE_1", "45 polegadas");
+            ResultSet result2 = insereProduto(statement, "PRODUTO_TESTE_2", "Radio de bolso");
+            ResultSet result3 = insereProduto(statement, "PRODUTO_TESTE_3", "Gabinete");
+            ResultSet result4 = insereProduto(statement, "PRODUTO_TESTE_4", "13 polegadas");
 
             // Se chegou até o final então
             assertNotNull(result1);
