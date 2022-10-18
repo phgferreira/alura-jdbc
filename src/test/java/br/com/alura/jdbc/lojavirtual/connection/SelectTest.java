d5cfea187f22351502db36290b0d236d958bc2cf
Paulo Ferreira <phgferreira@outlook.com>
2022-10-17 21:33:38 -0300

    Feito um teste de consulta ao banco de dados

---
+++ src/test/java/br/com/alura/jdbc/lojavirtual/connection/SelectTest.java
@@ -0,0 +1,58 @@
+package br.com.alura.jdbc.lojavirtual.connection;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SelectTest {
+
+    private Connection connection;
+
+    @BeforeEach
+    void beforeEach() {
+        String url = "jdbc:mysql://localhost/loja_virtual?userTimezone=true&serverTimezone=UTC";
+        String user = "dbauser";
+        String password = "p@ssw0rd";
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterEach
+    void afterEach() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test @DisplayName("Deve retornar uma consulta com sucesso")
+    void cenario1() {
+        String query = "select id_produto, nome, descricao from produto";
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            ResultSet result = statement.getResultSet();
+            while (result.next()) {
+                Integer id = result.getInt("id_produto");
+                String nome = result.getString("nome");
+                String descricao = result.getString("descricao");
+
+                assertEquals(1, id);
+                assertEquals("Notebook", nome);
+                assertEquals("Notebook Samsung", descricao);
+            }
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+    }
+}
